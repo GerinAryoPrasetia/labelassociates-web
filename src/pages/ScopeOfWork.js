0fb8041b05ce53635de6cc394d4224cee574cd14
GerinAryoPrasetia <gerinaryo14@gmail.com>
2022-07-18 21:50:22 +0700

    scope of work mobile layout

--- src/pages/ScopeOfWork.js
+++ src/pages/ScopeOfWork.js
@@ -98,157 +98,315 @@ const ScopeOfWork = () => {
   };
 
   return (
-    <div className={styles.container_work}>
-      <Navbar />
-      <div className={styles.main_content_about}>
-        <div class={styles.side_container}>
-          <div class={styles.side_content}>
-            {/* <h1>LabelAssociates</h1> */}
-            <div
-              className={styles.item_container}
-              onClick={() => {
-                sideClickedOne();
-              }}
-            >
-              <p
-                className={
-                  isActiveOne ? styles.side_item_active : styles.side_item
-                }
+    <>
+      <div className={styles.container_work}>
+        <Navbar />
+        <div className={styles.main_content_about}>
+          <div class={styles.side_container}>
+            <div class={styles.side_content}>
+              {/* <h1>LabelAssociates</h1> */}
+              <div
+                className={styles.item_container}
+                onClick={() => {
+                  sideClickedOne();
+                }}
               >
-                Type of Work
-              </p>
-            </div>
-            <div
-              className={styles.item_container}
-              onClick={() => {
-                sideClickedTwo();
-              }}
-            >
-              <p
-                className={
-                  isActiveTwo ? styles.side_item_active : styles.side_item
-                }
+                <p
+                  className={
+                    isActiveOne ? styles.side_item_active : styles.side_item
+                  }
+                >
+                  Type of Work
+                </p>
+              </div>
+              <div
+                className={styles.item_container}
+                onClick={() => {
+                  sideClickedTwo();
+                }}
               >
-                Work Flow
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* Content */}
-        <div className={styles.container_content_work}>
-          <div className="container-work">
-            <div
-              className={
-                overlayShowing ? "overlay-work" : "overlay-work-hidden"
-              }
-              onClick={() => hideOverlay()}
-            >
-              <div className="overlay-work-content">
-                <p>Type of Work</p>
+                <p
+                  className={
+                    isActiveTwo ? styles.side_item_active : styles.side_item
+                  }
+                >
+                  Work Flow
+                </p>
               </div>
             </div>
-            <div
-              className={
-                overlayShowing ? "slider-work work-hidden" : "slider-work "
-              }
-            >
-              <AiOutlineArrowLeft
-                className="arrow_work prev_work"
-                onClick={prevSlide}
-              />
-              <AiOutlineArrowRight
-                className="arrow_work next_work"
-                onClick={nextSlide}
-              />
-              {LISTTYPEWORK.map((container, index) => (
-                <div
-                  className={
-                    index === currentSlide
-                      ? "slide-work current-work"
-                      : "slide-work"
-                  }
-                  key={index}
-                >
+          </div>
+          {/* Content */}
+          <div className={styles.container_content_work}>
+            <div className="container-work">
+              <div
+                className={
+                  overlayShowing ? "overlay-work" : "overlay-work-hidden"
+                }
+                onClick={() => hideOverlay()}
+              >
+                <div className="overlay-work-content">
+                  <p>Type of Work</p>
+                </div>
+              </div>
+              <div
+                className={
+                  overlayShowing ? "slider-work work-hidden" : "slider-work "
+                }
+              >
+                <AiOutlineArrowLeft
+                  className="arrow_work prev_work"
+                  onClick={prevSlide}
+                />
+                <AiOutlineArrowRight
+                  className="arrow_work next_work"
+                  onClick={nextSlide}
+                />
+                {LISTTYPEWORK.map((container, index) => (
                   <div
-                    className="img-work"
-                    style={{
-                      backgroundImage: `url(${container.img})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                    }}
+                    className={
+                      index === currentSlide
+                        ? "slide-work current-work"
+                        : "slide-work"
+                    }
+                    key={index}
                   >
-                    1
+                    <div
+                      className="img-work"
+                      style={{
+                        backgroundImage: `url(${container.img})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                    >
+                      1
+                    </div>
+                    <div className="content-work">
+                      <h3>{container.name}</h3>
+                      <ul>
+                        {container.data.map((c, idx) => (
+                          <ul>{c}</ul>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
-                  <div className="content-work">
-                    <h3>{container.name}</h3>
-                    <ul>
-                      {container.data.map((c, idx) => (
-                        <ul>{c}</ul>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="container-work">
-            <div
-              className={
-                overlayShowingTwo ? "overlay-work" : "overlay-work-hidden"
-              }
-              onClick={() => hideOverlayTwo()}
-            >
-              <div className="overlay-work-content">
-                <p>Work Flow</p>
+                ))}
               </div>
             </div>
-            <div
-              className={
-                overlayShowingTwo ? "slider-work  work-hidden " : "slider-work"
-              }
-            >
-              <AiOutlineArrowLeft
-                className="arrow_work prev_work"
-                onClick={prevSlideTwo}
-              />
-              <AiOutlineArrowRight
-                className="arrow_work next_work"
-                onClick={nextSlideTwo}
-              />
-              {LISTWORKFLOW.map((container, index) => (
-                <div
-                  className={
-                    index === currentSlideTwo
-                      ? "slide-work current-work"
-                      : "slide-work"
-                  }
-                  key={index}
-                >
-                  <div
-                    className="img-work"
-                    style={{
-                      backgroundImage: `url(${container.img})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                    }}
-                  ></div>
-                  <div className="content-work">
-                    <h3>
-                      {index + 1}.{container.name}
-                    </h3>
-                    <ul>
-                      {container.data.map((c, idx) => (
-                        <ul>{c}</ul>
-                      ))}
-                    </ul>
-                  </div>
+            <div className="container-work">
+              <div
+                className={
+                  overlayShowingTwo ? "overlay-work" : "overlay-work-hidden"
+                }
+                onClick={() => hideOverlayTwo()}
+              >
+                <div className="overlay-work-content">
+                  <p>Work Flow</p>
                 </div>
-              ))}
+              </div>
+              <div
+                className={
+                  overlayShowingTwo
+                    ? "slider-work  work-hidden "
+                    : "slider-work"
+                }
+              >
+                <AiOutlineArrowLeft
+                  className="arrow_work prev_work"
+                  onClick={prevSlideTwo}
+                />
+                <AiOutlineArrowRight
+                  className="arrow_work next_work"
+                  onClick={nextSlideTwo}
+                />
+                {LISTWORKFLOW.map((container, index) => (
+                  <div
+                    className={
+                      index === currentSlideTwo
+                        ? "slide-work current-work"
+                        : "slide-work"
+                    }
+                    key={index}
+                  >
+                    <div
+                      className="img-work"
+                      style={{
+                        backgroundImage: `url(${container.img})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                    ></div>
+                    <div className="content-work">
+                      <h3>
+                        {index + 1}.{container.name}
+                      </h3>
+                      <ul>
+                        {container.data.map((c, idx) => (
+                          <ul>{c}</ul>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* mobile */}
+      <div className={styles.container_work_mobile}>
+        <Navbar />
+        <div className={styles.main_content_about_mobile}>
+          <div class={styles.side_container_mobile}>
+            <div class={styles.side_content_mobile}>
+              {/* <h1>LabelAssociates</h1> */}
+              <div
+                className={styles.item_container}
+                onClick={() => {
+                  sideClickedOne();
+                }}
+              >
+                <p
+                  className={
+                    isActiveOne ? styles.side_item_active : styles.side_item
+                  }
+                >
+                  Type of Work
+                </p>
+              </div>
+              <div
+                className={styles.item_container}
+                onClick={() => {
+                  sideClickedTwo();
+                }}
+              >
+                <p
+                  className={
+                    isActiveTwo ? styles.side_item_active : styles.side_item
+                  }
+                >
+                  Work Flow
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Content */}
+          <div className={styles.container_content_work}>
+            <div className="container-work">
+              <div
+                className={
+                  overlayShowing ? "overlay-work" : "overlay-work-hidden"
+                }
+                onClick={() => hideOverlay()}
+              >
+                <div className="overlay-work-content">
+                  <p>Type of Work</p>
+                </div>
+              </div>
+              <div
+                className={
+                  overlayShowing ? "slider-work work-hidden" : "slider-work "
+                }
+              >
+                <AiOutlineArrowLeft
+                  className="arrow_work prev_work"
+                  onClick={prevSlide}
+                />
+                <AiOutlineArrowRight
+                  className="arrow_work next_work"
+                  onClick={nextSlide}
+                />
+                {LISTTYPEWORK.map((container, index) => (
+                  <div
+                    className={
+                      index === currentSlide
+                        ? "slide-work current-work"
+                        : "slide-work"
+                    }
+                    key={index}
+                  >
+                    <div
+                      className="img-work"
+                      style={{
+                        backgroundImage: `url(${container.img})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                    >
+                      1
+                    </div>
+                    <div className="content-work">
+                      <h3>{container.name}</h3>
+                      <ul>
+                        {container.data.map((c, idx) => (
+                          <ul>{c}</ul>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="container-work">
+              <div
+                className={
+                  overlayShowingTwo ? "overlay-work" : "overlay-work-hidden"
+                }
+                onClick={() => hideOverlayTwo()}
+              >
+                <div className="overlay-work-content">
+                  <p>Work Flow</p>
+                </div>
+              </div>
+              <div
+                className={
+                  overlayShowingTwo
+                    ? "slider-work  work-hidden "
+                    : "slider-work"
+                }
+              >
+                <AiOutlineArrowLeft
+                  className="arrow_work prev_work"
+                  onClick={prevSlideTwo}
+                />
+                <AiOutlineArrowRight
+                  className="arrow_work next_work"
+                  onClick={nextSlideTwo}
+                />
+                {LISTWORKFLOW.map((container, index) => (
+                  <div
+                    className={
+                      index === currentSlideTwo
+                        ? "slide-work current-work"
+                        : "slide-work"
+                    }
+                    key={index}
+                  >
+                    <div
+                      className="img-work"
+                      style={{
+                        backgroundImage: `url(${container.img})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                    ></div>
+                    <div className="content-work">
+                      <h3>
+                        {index + 1}.{container.name}
+                      </h3>
+                      <ul>
+                        {container.data.map((c, idx) => (
+                          <ul>{c}</ul>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
